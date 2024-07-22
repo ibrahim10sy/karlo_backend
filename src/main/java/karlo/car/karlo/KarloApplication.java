c384184fc79bdbcb939a1e9a52587198e557b98b
ibrahim10sy <ibrahim10sy@gmail.com>
2024-07-22 10:35:57 +0000

    first commit

---
+++ src/main/java/karlo/car/karlo/KarloApplication.java
@@ -0,0 +1,13 @@
+package karlo.car.karlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/karlo/car/karlo/KarloApplicationTests.java
@@ -0,0 +1,13 @@
+package karlo.car.karlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
